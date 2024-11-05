ce2308a03af47a41d0c2a0c8f53dd1e34285b7e7
Ali Zamanian <Zamanian@hovita.local>
2024-11-05 15:22:00 +0330

    Done

--- src/main/java/org/example/Countinuegetvalue.java
+++ src/main/java/org/example/Countinuegetvalue.java
@@ -3,21 +3,24 @@ package org.example;
 import java.util.Scanner;
 
 public class Countinuegetvalue {
-   public static void main(String[] args){
+    public static void main(String[] args) {
 
 
-       Scanner scanner= new Scanner(System.in);
-       int clientNumber = scanner.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        int clientNumber = scanner.nextInt();
+        System.out.println("Continue Untill you press 0 ");
 
-       while (true) {
-           System.out.println("Continue Untill you press 0 ");
-           clientNumber = scanner.nextInt();
-           if (clientNumber==0)
-               break;
-           scanner.close();
-       }
+        while (true) {
+            clientNumber = scanner.nextInt();
+            if (clientNumber == 0){
+                break;
+            }
+            System.out.println("Continue Untill you press 0 ");
+
+        }
 
 
+        scanner.close();
 
-   }
+    }
 }
